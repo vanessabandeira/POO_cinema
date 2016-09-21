b022f85f18e7ba2a5638e424f084ae8e5e50e5d6
PedroAntônio <PedroAntnio@Peu-PC>
2016-09-20 23:30:19 -0300

    Inserindo Atributos das Classes Básicas: Sala ^ Filme

--- src/classes/Sala.java
+++ src/classes/Sala.java
@@ -1,10 +1,10 @@
 package classes;
 
-public class Salas {
+public class Sala{
 	private Filme[] filmesdasala;
 	private int capacidade;
 	private int identidade;
 	private boolean v3d;
 	private boolean v4k;
-	private boolean prime; //DISCUTIR NOME DE VARIÁVEL COM PEDRO
+	private boolean prime;
 }
