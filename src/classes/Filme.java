f52357136bb57a6201319263cb05ac6f846468b9
saulo.gomesferreira <saulo.gomesferreira@JVS33-14.labdeinfo.ufrpe>
2016-09-21 17:22:34 -0300

    Implementação dos métodos inserir e pesquisarnome do RepositorioFilmes e get e set da classe Filme e RepositorioFilmes.

--- src/classes/Filme.java
+++ src/classes/Filme.java
@@ -1,9 +1,40 @@
 package classes;
 
 public class Filme {
-	private String título;
-	private int classificação;
-	private String gênero;
-	private int duração;
+	private String titulo;
+	private int classificacao;
+	private String genero;
+	private int duracao;
 	private int nvendas;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getClassificacao() {
+		return classificacao;
+	}
+	public void setClassificacao(int classificacao) {
+		this.classificacao = classificacao;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getDuracao() {
+		return duracao;
+	}
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+	public int getNvendas() {
+		return nvendas;
+	}
+	public void setNvendas(int nvendas) {
+		this.nvendas = nvendas;
+	}
+	
 }
--- src/classes/RepositorioFilmes.java
+++ src/classes/RepositorioFilmes.java
@@ -1,5 +1,65 @@
 package classes;
 
 public class RepositorioFilmes {
+	private Filme[] filmes;
+	private int numerofilmes;
+	
+	public RepositorioFilmes(int tam){
+		this.filmes = new Filme[tam];
+		numerofilmes = 0;
+	}
+	public boolean inserir(Filme filme){
+		if(filme != null){
+			Filme search = new Filme();
+			search = pesquisarnome(filme);
+			if(search==null && numerofilmes==100 && filme.getTitulo() != null){
+				this.filmes[numerofilmes].setTitulo(filme.getTitulo());
+				this.filmes[numerofilmes].setClassificacao(filme.getClassificacao());
+				this.filmes[numerofilmes].setGenero(filme.getGenero());
+				this.filmes[numerofilmes].setDuracao(filme.getDuracao());
+				this.numerofilmes++;
+				return true;
+			}
+		}
+		return false;
+	}
+	public boolean remover(Filme filme){
+		
+	}
+	public Filme pesquisarnome(Filme filme){
+		int i;
+		for(i=0; i<numerofilmes; i++){
+			if(this.filmes[i].getTitulo().equals(filmes[i].getTitulo())){
+				return filmes[i];
+			}
+		}
+		return null;
+	}
+	public Filme pesquisargenero(Filme filme){
+		
+	}
+	public boolean alterar(Filme filme){
+		
+	}
+	void listar(){
+		
+	}
+	
+	public Filme[] getFilmes() {
+		return filmes;
+	}
+	
+	public void setFilmes(Filme[] filmes) {
+		this.filmes = filmes;
+	}
+	
+	public int getNumerofilmes() {
+		return numerofilmes;
+	}
+	
+	public void setNumerofilmes(int numerofilmes) {
+		this.numerofilmes = numerofilmes;
+	}
+	
 
 }
