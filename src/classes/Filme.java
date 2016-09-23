b3cf328efcddc6d71a6e5bb5b7ff0fdc044b2fdd
pedro.antonio <pedro.antonio@JVS33-13.labdeinfo.ufrpe>
2016-09-23 15:02:52 -0300

    Mudança apenas na organização do código.

--- src/classes/Filme.java
+++ src/classes/Filme.java
@@ -6,35 +6,45 @@ public class Filme {
 	private String genero;
 	private int duracao;
 	private int nvendas;
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public int getClassificacao() {
 		return classificacao;
 	}
+
 	public void setClassificacao(int classificacao) {
 		this.classificacao = classificacao;
 	}
+
 	public String getGenero() {
 		return genero;
 	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+
 	public int getDuracao() {
 		return duracao;
 	}
+
 	public void setDuracao(int duracao) {
 		this.duracao = duracao;
 	}
+
 	public int getNvendas() {
 		return nvendas;
 	}
+
 	public void setNvendas(int nvendas) {
 		this.nvendas = nvendas;
 	}
-	
+
 }
--- src/classes/Sessao.java
+++ src/classes/Sessao.java
@@ -1,5 +1,22 @@
 package classes;
 
 public class Sessao {
+	private int[] Horario;
+	public Filme Exibição;
+	private int[] Data; //SERÁ SUBSTITUIDO POR UM OBJETO DO TIPO LocalDate da biblioteca Java.time
+	private Sala Hall;
+	private int ocupação;
+	private float valor; // valor do ingresso inteiro
+	private int nvInteiro; // número de vendas de ingressos inteiros
+	private int nvMeia; // número de vendas de meio ingressos
+	private boolean[] posição; // as cadeiras da sala
 
+	/*
+	 * public Sessao{
+	 *  MÉTODO CONSTRUTOR
+	 * }
+	 */
+	public void ocuparcadeira(int Poltrona) {
+		this.posição[Poltrona] = false;
+	}
 }
