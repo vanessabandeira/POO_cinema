46b1bebd6eaf3e73e857e9f198d92376f3b53c5c
pedro.antonio <pedro.antonio@JVS33-13.labdeinfo.ufrpe>
2016-09-23 15:29:01 -0300

    Sessão+RepositorioSessão

--- src/classes/RepositorioSessao.java
+++ src/classes/RepositorioSessao.java
@@ -1,43 +1,47 @@
 package classes;
 
 public class RepositorioSessao {
-	private Sessao[] Sessões; //serve para mostrar quais os filmes disponíveis e em quais horários e salas
+	private Sessao[] Sessões; // serve para mostrar quais os filmes disponíveis
+								// e em quais horários e salas
 	private int nSessões;
-	
-	
+
 	/*
-	 * public RepositorioSessao{
-	 *  MÉTODO CONSTRUTOR
-	 * }
+	 * public RepositorioSessao{ MÉTODO CONSTRUTOR }
 	 */
-	
-	public boolean inserir(Sessao Sessão){
-		//A desenvolver
-		//Insere uma Sessão no Array de sessões 
+
+	public boolean inserir(Sessao Sessão) {
+		// A desenvolver
+		// Insere uma Sessão no Array de sessões
 	}
-	public boolean remover(Sessão Sessão){
-		//A desenvolver
-		//Remove uma sessão do array
+
+	public boolean remover(Sessão Sessão) {
+		// A desenvolver
+		// Remove uma sessão do array
 	}
-	public void pesquisarHorario(/* HoraInicio, HoraFim */){
-		//recebe horario de inicio e fim e imprime sessões disponíveis entre o período,
-		//caso horafim seja NULL, imprimirá todas as sessões disponíveis após o horario de inicio
-		//HoraInício não pode ser NULL
+
+	public void pesquisarHorario(/* HoraInicio, HoraFim */) {
+		// recebe horario de inicio e fim e imprime sessões disponíveis entre o
+		// período,
+		// caso horafim seja NULL, imprimirá todas as sessões disponíveis após o
+		// horario de inicio
+		// HoraInício não pode ser NULL
 	}
+
 	public Sessão pesquisarFilme(Filme){
 		//retorna sessão que contenha o filme especificado
 	}
-	
-	public Sessão pesquisarSala(int identidade){
-		//retorna a sessão atual da sala pesquisada;
+
+	public Sessão pesquisarSala(int identidade) {
+		// retorna a sessão atual da sala pesquisada;
 	}
-	
-	public boolean alterar(Sessao sessao){
-		//altera inforçoes de uma sessao, retornando verdadeiro caso ela exista. Retorna falso, caso a sessão não exista
+
+	public boolean alterar(Sessao sessao) {
+		// altera inforçoes de uma sessao, retornando verdadeiro caso ela
+		// exista. Retorna falso, caso a sessão não exista
 	}
-	
-	public void listar(){
-		//Imprime todas as sessões disponiveis;
+
+	public void listar() {
+		// Imprime todas as sessões disponiveis;
 	}
-	
+
 }
--- src/classes/Sessao.java
+++ src/classes/Sessao.java
@@ -3,7 +3,8 @@ package classes;
 public class Sessao {
 	private int[] Horario;
 	public Filme Exibição;
-	private int[] Data; //SERÁ SUBSTITUIDO POR UM OBJETO DO TIPO LocalDate da biblioteca Java.time
+	private int[] Data; // SERÁ SUBSTITUIDO POR UM OBJETO DO TIPO LocalDate da
+						// biblioteca Java.time
 	private Sala Hall;
 	private int ocupação;
 	private float valor; // valor do ingresso inteiro
@@ -12,9 +13,7 @@ public class Sessao {
 	private boolean[] posição; // as cadeiras da sala
 
 	/*
-	 * public Sessao{
-	 *  MÉTODO CONSTRUTOR
-	 * }
+	 * public Sessao{ MÉTODO CONSTRUTOR }
 	 */
 	public void ocuparcadeira(int Poltrona) {
 		this.posição[Poltrona] = false;
