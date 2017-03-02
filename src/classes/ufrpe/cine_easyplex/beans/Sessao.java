066dfcc7f528c325fb3a8615d5175d7e37afe57d
root <root@Cyan-PC>
2017-03-01 22:21:28 -0300

    Inserindo os DATEs

--- src/classes/ufrpe/cine_easyplex/beans/Sessao.java
+++ src/classes/ufrpe/cine_easyplex/beans/Sessao.java
@@ -1,13 +1,10 @@
 package classes.ufrpe.cine_easyplex.beans;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Date;
 
 public class Sessao {
-	private LocalTime horario;
+	private Date horario; //inicio da sessao
 	public Filme exibicao;
-	private LocalDate data; // SER� SUBSTITUIDO POR UM OBJETO DO TIPO LocalDate da
-						// biblioteca Java.time
 	private Sala hall;
 	private int ocupacao;
 	private float valor; // valor do ingresso inteiro
@@ -18,80 +15,95 @@ public class Sessao {
 	/*
 	 * public Sessao{ M�TODO CONSTRUTOR }
 	 */
-	public Sessao(LocalTime horario, Filme exibicao, LocalDate data, Sala hall){
-		this.exibicao=exibicao;
-		this.horario=horario;
-		this.data=data;
-		this.hall=hall;
+	public Sessao(Date horario, Filme exibicao, Sala hall) {
+		this.exibicao = exibicao;
+		this.horario = horario;
+		this.hall = hall;
 		this.posicao = new boolean[hall.getCapacidade()];
-		this.nvInteiro=0;
-		this.nvMeia=0;
-		for(int i = 0; i<hall.getCapacidade(); i++){
-			this.posicao[i]=true;
+		this.nvInteiro = 0;
+		this.nvMeia = 0;
+		for (int i = 0; i < hall.getCapacidade(); i++) {
+			this.posicao[i] = true;
 		}
 	}
+
 	public void ocuparcadeira(int Poltrona) {
 		this.posicao[Poltrona] = false;
 	}
-	public LocalTime getHorario() {
-		return horario;
+
+	public String getHorario() {
+		return this.horario.toString();
 	}
-	public void setHorario(LocalTime horario) {
+
+	public void setHorario(Date horario) {
 		this.horario = horario;
 	}
+
 	public Filme getExibicao() {
 		return exibicao;
 	}
+
 	public void setExibicao(Filme exibicao) {
 		this.exibicao = exibicao;
 	}
+
 	public Sala getHall() {
 		return hall;
 	}
+
 	public void setHall(Sala hall) {
 		this.hall = hall;
 	}
+
 	public int getOcupacao() {
 		return ocupacao;
 	}
+
 	public void setOcupacao(int ocupacao) {
 		this.ocupacao = ocupacao;
 	}
+
 	public float getValor() {
 		return valor;
 	}
+
 	public void setValor(float valor) {
 		this.valor = valor;
 	}
+
 	public int getNvInteiro() {
 		return nvInteiro;
 	}
+
 	public void setNvInteiro(int nvInteiro) {
 		this.nvInteiro = nvInteiro;
 	}
+
 	public int getNvMeia() {
 		return nvMeia;
 	}
+
 	public void setNvMeia(int nvMeia) {
 		this.nvMeia = nvMeia;
 	}
+
 	public boolean[] getPosicao() {
 		return posicao;
 	}
+
 	public void setPosicao(boolean[] posicao) {
 		this.posicao = posicao;
 	}
-	public boolean equals(Sessao comparada){
-		if (this.exibicao.equals(comparada.exibicao)){
-			if (this.horario.equals(comparada.horario)){
-				if(this.data.equals(comparada.data)){
-					if(this.hall.equals(comparada.hall)){
-						return true;
-					}
+
+	public boolean equals(Sessao comparada) {
+		if (this.exibicao.equals(comparada.exibicao)) {
+			if (this.horario.equals(comparada.horario)) {
+				if (this.hall.equals(comparada.hall)) {
+					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
+
 }
