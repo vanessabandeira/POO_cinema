6524b2dc79e36d034df7c10ec84ca74d48d37e65
Vanessa Bandeira <van3ssabandeira@gmail.com>
2017-03-11 21:04:20 -0300

    passwordfield

--- src/classes/ufrpe/cine_easyplex/GUI/TelaLoginController.java
+++ src/classes/ufrpe/cine_easyplex/GUI/TelaLoginController.java
@@ -1,12 +1,13 @@
 package classes.ufrpe.cine_easyplex.GUI;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class TelaLoginController {
 	
 	@FXML TextField tfLogin;
-	@FXML TextField tfSenha;
+	@FXML PasswordField tfSenha;
 	
 	public void Login(){
 		
