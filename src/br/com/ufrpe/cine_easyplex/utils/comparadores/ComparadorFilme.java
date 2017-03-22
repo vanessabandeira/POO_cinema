56619d0cdc6bc3250ad3d57d4bd11eba7afbbe28
Pedro <Pedro@pc-lenovo.home>
2017-03-22 01:47:51 -0300

    correção comparator Filme

--- src/br/com/ufrpe/cine_easyplex/utils/comparadores/ComparadorFilme.java
+++ src/br/com/ufrpe/cine_easyplex/utils/comparadores/ComparadorFilme.java
@@ -8,7 +8,7 @@ public class ComparadorFilme implements Comparator<Filme>{
 
 	@Override
 	public int compare(Filme arg0, Filme arg1) {
-		return arg0.getTitulo().compareTo(arg1.getTitulo());
+		return arg0.toString().compareTo(arg1.toString());
 	}
 
 }
